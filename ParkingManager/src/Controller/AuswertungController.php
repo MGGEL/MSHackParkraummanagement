0eafe5ad134888e6d08cd557c433aaae6373e5d1
M.Gorgemann <gorgemann@energielenker.de>
2020-09-26 11:30:31 +0200

    Auslastung 1

---
+++ ParkingManager/src/Controller/AuswertungController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Parkplatz;
+use App\Repository\ParkplatzRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Energielenker\LoraBundle\Entity\Device;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuswertungController extends AbstractController
+{
+    private $parkplatzRepository;
+    private $entityManager;
+
+    public function __construct(ParkplatzRepository $parkplatzRepository,
+                                EntityManagerInterface $entityManager)
+    {
+        $this->parkplatzRepository = $parkplatzRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    public function Auslastung(): Response
+    {
+
+        return $this->render('Default/auslastung.twig');
+    }
+
+}
