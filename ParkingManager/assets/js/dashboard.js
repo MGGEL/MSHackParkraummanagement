ece20b64b36e7a44de384d8b6a5b0bf81e8be71c
Holger Schnieders <schnieders@energielenker.de>
2020-09-26 15:50:19 +0200

    Adds custom images

--- ParkingManager/assets/js/dashboard.js
+++ ParkingManager/assets/js/dashboard.js
@@ -10,41 +10,157 @@ document.addEventListener('DOMContentLoaded', function () {
         styles: getStyle()
     });
 
+    let iconBase = '/build/static/images/pins/';
+
+    var images = {
+        parking: {
+            icon: iconBase + 'Pin2_edit.png'
+        },
+        wrongParking: {
+            icon: iconBase + 'Pin1_edit.png'
+        }
+    };
+
     var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
     let positions = [
-        {lat: 51.959903, lng: 7.626327},
-        {lat: 51.960167, lng: 7.623173},
-        {lat: 51.962124, lng: 7.616864},
-        {lat: 51.964546, lng: 7.616972},
-        {lat: 51.965966, lng: 7.626799},
-        {lat: 51.964525, lng: 7.632045},
-        {lat: 51.963937, lng: 7.632206},
-        {lat: 51.961137, lng: 7.630178},
-        {lat: 51.960857, lng: 7.630092},
-        {lat: 51.949955, lng: 7.636715},
-        {lat: 51.970162, lng: 7.622208},
-        {lat: 51.970418, lng: 7.623847},
-        {lat: 51.969843, lng: 7.623997},
-        {lat: 51.969763, lng: 7.620070},
-        {lat: 51.970050, lng: 7.617313},
-        {lat: 51.961906, lng: 7.616402},
-        {lat: 51.952240, lng: 7.614321},
-        {lat: 51.947175, lng: 7.622110},
-        {lat: 51.942784, lng: 7.629126},
-        {lat: 51.938980, lng: 7.616917},
-        {lat: 51.930540, lng: 7.631980},
-        {lat: 51.939007, lng: 7.657284},
-        {lat: 51.950885, lng: 7.669987},
-        {lat: 51.981081, lng: 7.676725},
-        {lat: 51.965747, lng: 7.653293},
-        {lat: 51.951105, lng: 7.637412}
+        {
+            position: {
+                lat: 51.959903, lng: 7.626327
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.960167, lng: 7.623173
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.962124, lng: 7.616864
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.964546, lng: 7.616972
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.965966, lng: 7.626799
+            }, type: 'wrongParking'
+        },
+        {
+            position: {
+                lat: 51.964525, lng: 7.632045
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.963937, lng: 7.632206
+            }, type: 'wrongParking'
+        },
+        {
+            position: {
+                lat: 51.961137, lng: 7.630178
+            }, type: 'wrongParking'
+        },
+        {
+            position: {
+                lat: 51.960857, lng: 7.630092
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.949955, lng: 7.636715
+            }, type: 'wrongParking'
+        },
+        {
+            position: {
+                lat: 51.970162, lng: 7.622208
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.970418, lng: 7.623847
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.969843, lng: 7.623997
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.969763, lng: 7.620070
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.970050, lng: 7.617313
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.961906, lng: 7.616402
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.952240, lng: 7.614321
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.947175, lng: 7.622110
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.942784, lng: 7.629126
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.938980, lng: 7.616917
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.930540, lng: 7.631980
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.939007, lng: 7.657284
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.950885, lng: 7.669987
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.981081, lng: 7.676725
+            }, type: 'parking'
+        },
+        {
+            position: {
+                lat: 51.965747, lng: 7.653293
+            }, type: 'wrongParking'
+        },
+        {
+            position: {
+                lat: 51.951105, lng: 7.637412
+            }, type: 'parking'
+        }
     ];
 
     let markers = positions.map(function(position, i) {
         return new google.maps.Marker({
-            position: position,
-            label: labels[i % labels.length]
+            position: new google.maps.LatLng(position.position.lat, position.position.lng),
+            // label: labels[i % labels.length],
+            icon: images[position.type].icon,
         });
     });
 
